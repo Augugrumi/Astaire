145d7db0702377fd9cc3838a6e86eaba643b44d1
mzanella <zanna0150@gmail.com>
2018-09-17 09:50:52 +0200

    Perform refactor of handler hierarchy and add java handler

---
+++ src/connection/handler/javahandler.h
@@ -0,0 +1,34 @@
+//
+// Created by zanna on 17/09/18.
+//
+
+#ifndef ASTAIRE_JAVAHANDLER_H
+#define ASTAIRE_JAVAHANDLER_H
+
+#include <jni.h>
+#include <iostream>
+#include <stdio.h>
+#include <sstream>
+#include <string>
+
+#include "abshandler.h"
+
+namespace connection{
+
+    namespace handler{
+
+        class JavaHandler : public AbsHandler {
+        private:
+            unsigned char* execute_java(const std::string& class_file_path,
+                    const std::string& class_name,
+                    const std::string& method_name,
+                    unsigned char* pkt,
+                    std::size_t pkt_size) const;
+        public:
+            JavaHandler(const std::string& config_path);
+            void handler_request(unsigned char* message, std::size_t size) const;
+        };
+    } // namespace handler
+} // namespace connection
+
+#endif //ASTAIRE_JAVAHANDLER_H
