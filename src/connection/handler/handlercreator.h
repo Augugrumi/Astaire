ea79c6b3249952d6205c7d4791379df1fa5a5da8
mzanella <zanna0150@gmail.com>
2018-09-17 10:34:01 +0200

    Add handler creator

---
+++ src/connection/handler/handlercreator.h
@@ -0,0 +1,26 @@
+//
+// Created by zanna on 17/09/18.
+//
+
+#ifndef ASTAIRE_HANDLERCREATOR_H
+#define ASTAIRE_HANDLERCREATOR_H
+
+#include <string>
+#include <boost/algorithm/string.hpp>
+
+#include "log.h"
+#include "handler/abshandler.h"
+#include "handler/javahandler.h"
+
+namespace connection {
+    namespace handler {
+
+        class HandlerCreator {
+        public:
+            static AbsHandler* getHandlerByLanguageName(const std::string & language, const std::string & config_file);
+        };
+
+    } // namespace handler
+} // namespace connection
+
+#endif //ASTAIRE_HANDLERCREATOR_H
