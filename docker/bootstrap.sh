60fb0b0d18317898943888b532830aef333d5f32
Polonio Davide <poloniodavide@gmail.com>
2018-09-24 15:58:46 +0200

    Using echo and tac instead of sed

--- docker/bootstrap.sh
+++ docker/bootstrap.sh
@@ -4,7 +4,10 @@ set -e
 
 if [ -n "$KUBERNETES_PORT" ] && [ -n "$DNS_IP" ]
 then
-    sed -i "1 i\nameserver $DNS_IP" /etc/resolv.conf
+    tmpfile=$(mktemp)
+    (echo "nameserver $DNS_IP" && tac /etc/resolv.conf) > $tmpfile
+    cat $tmpfile > /etc/resolv.conf
+    rm $tmpfile
 fi
 
 astaire $@
